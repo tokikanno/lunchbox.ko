892d56ba1460eb6889b1d89fcfcb54e405e1aa81
toki <toki.kanno@gmail.com>
2013-06-23 23:17:10 +0800

    change to web.py

--- lunchbox.py
+++ lunchbox.py
@@ -1,5 +1,5 @@
 import web
-from json import dumps, loads
+from bson.json_util import dumps, loads
 
 from webpy_mongodb_sessions.session import MongoStore
 from pymongo import Connection
@@ -10,15 +10,22 @@ from jinja2 import Environment,FileSystemLoader
 MONGODB_CONNECTION = Connection()
 DB = Connection()['lunchbox']
 
-APP_SECRET = '/]\xbf\xe2f\xb0\x11\x89\xd5\xe8\xfdA\xd2j_\x8aww!\x11~r\xc2\x9b'
+APP_SECRET = u'e18ec3fb1fb059fa06a0e25fc8e95397'
 
 urls = (
 	'/', 'index',
 	'/login', 'login',
+	'/logout', 'logout',
+	'/order/active', 'active_order',
 )
 
 app = web.application(urls, globals())
-session = web.session.Session(app, MongoStore(MONGODB_CONNECTION['lunchbox'], 'sessions'))
+
+if web.config.get('_session') is None:
+    session = web.session.Session(app, MongoStore(MONGODB_CONNECTION['lunchbox'], 'sessions'))
+    web.config._session = session
+else:
+    session = web.config._session
 
 def render_template(template_name, **context):
     extensions = context.pop('extensions', [])
@@ -46,6 +53,14 @@ def load_json():
 		return None
 
 
+def get_user(find_filter=None):
+	userid = session.get('userid')
+	if not userid:
+		return None
+
+	return DB['user'].find_one({'_id': userid}, find_filter)
+
+
 def gen_password(password):
 	from md5 import md5
 	m = md5()
@@ -55,11 +70,18 @@ def gen_password(password):
 
 class index:
 	def GET(self):
-		if not 'userid' in session:
+		user = get_user({'username': 1})
+
+		if not user:
 			raise web.seeother('/login')
 		else:
-			return render_template("app.html")
+			user['email'] = user['_id']
+			return render_template("app.html", user=user)
 
+class logout:
+	def GET(self):
+		session.kill()
+		raise web.seeother('/')
 
 class login:
 	def GET(self):
@@ -75,17 +97,18 @@ class login:
 		try:
 			data = load_json()
 			assert data, "bad POST data"
-			username = data.get('username').trim()
-			password = data.get('password').trim()
+			username = data.get('username').strip()
+			password = data.get('password').strip()
 			assert username, "no username found"
-			assert password in data, "no password found"
+			assert password, "no password found"
 			assert username.endswith('hopebaytech.com') or username.endswith('happygorgi.com'), "bad username"
 
 			user_collection = DB['user']
-			user = user_collection.find_one({'_id': username}, {'_id': 1, 'password': 1}):
+			user = user_collection.find_one({'_id': username}, {'_id': 1, 'password': 1})
+
 			if user:
 				# old user, verify password
-				assert gen_password(password) == password, "bad password"
+				assert gen_password(password) == user.get('password'), "bad password"
 			else:
 				# new user, add it
 				user = {
@@ -102,6 +125,9 @@ class login:
 
 		return json_resp(result)
 
+class active_order:
+	def GET(self):
+		return json_resp(list(DB['order'].find({'active': True})))
 
 if __name__ == "__main__":
 	app.debug = True
